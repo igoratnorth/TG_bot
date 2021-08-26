9783fe83d555016036c9022e5087645fbabaf5b8
igor-ok1 <igor-ok1@list.ru>
2021-08-26 23:00:47 +0300

    Создано подключение к ТГ получаем ID И Имя бота

--- TG_bot/Program.cs
+++ TG_bot/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace TG_bot
 {
     class Program
     {
+        public static ITelegramBotClient tg_client_test1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tg_client_test1 = new TelegramBotClient("1999658156:AAHqszUUXtrxL3CayPYUS119rpUXPIZTwQs") { Timeout= TimeSpan.FromSeconds(10)};
+            var inst1 = tg_client_test1.GetMeAsync().Result;
+            Console.WriteLine($"id:{ inst1.Id} name:{inst1.Username}");
+
+        
         }
     }
 }
