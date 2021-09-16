6e6595b6d3913f07374bc23d6f57db42bbea7079
igor-ok1 <igor-ok1@list.ru>
2021-09-16 15:01:48 +0300

    В core 5 нет system.servicemodel =(   Заменил нугетами. Сгенерил прокси классы сервиса.

--- TG_bot/Program.cs
+++ TG_bot/Program.cs
@@ -1,6 +1,14 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using ServiceReference1;
+
+
+
 
 namespace TG_bot
 {
@@ -27,4 +35,245 @@ namespace TG_bot
             await tg_client_test1.SendTextMessageAsync(chatId:e.Message.Chat, text: $"Привет человек! что такое {e.Message.Text}");
         }
     }
+    class TestClient
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            OTWebService webSvc = new OTWebService();
+
+            GetObjectListData gold = new GetObjectListData();
+
+
+
+            // Replace the following lines with your code that
+
+            // creates the request
+
+            gold.folderPath = "ChangeMgmt\\RFCs";
+
+            gold.recursive = true;
+
+            gold.RequiredField = new RequiredField[5];
+
+            gold.RequiredField[0] = new RequiredField();
+
+            gold.RequiredField[0].Value = "Title";
+
+            gold.RequiredField[1] = new RequiredField();
+
+            gold.RequiredField[1].Value = "Attachments";
+
+            gold.RequiredField[2] = new RequiredField();
+
+            gold.RequiredField[2].Value = "Last Change";
+
+            gold.RequiredField[3] = new RequiredField();
+
+            gold.RequiredField[3].Value = "Priority";
+
+            gold.RequiredField[4] = new RequiredField();
+
+            gold.RequiredField[4].Value = "Answers";
+
+
+
+            // Optional: use a filter
+
+            Filter flt = new Filter();
+
+            flt.name = "Open RFCs";
+
+            gold.Item = flt;
+
+
+
+            try
+
+            {
+
+                //simply call the proxy for the intended web method
+
+                GetObjectListResult golr = webSvc.GetObjectList(gold);
+
+
+
+                //Do something with the results
+
+                if (golr.success)
+
+                {
+
+                    ObjectData[] myObjects = golr.Object;
+
+                    for (int i = 0; i < myObjects.Length; i++)
+
+                    {
+
+                        foreach (object myItem in myObjects[i].Items)
+
+                        {
+
+                            if (myItem is NullVal)
+
+                            {
+
+                                string field = ((StringVal)myItem).name;
+
+                                //do something...
+
+                            }
+
+                            else if (myItem is StringVal)
+
+                            {
+
+                                string field = ((StringVal)myItem).name;
+
+                                string val = ((StringVal)myItem).Value;
+
+                                //do something...
+
+                            }
+
+                            else if (myItem is DateTimeVal)
+
+                            {
+
+                                string field = ((DateTimeVal)myItem).name;
+
+                                DateTime dt = ((DateTimeVal)myItem).Value;
+
+
+
+                                //do something...
+
+                            }
+
+                            else if (myItem is LongIntVal)
+
+                            {
+
+                                string field = ((LongIntVal)myItem).name;
+
+                                int val = ((LongIntVal)myItem).Value;
+
+
+
+                                //do something...
+
+                            }
+
+                            else if (myItem is ShortIntVal)
+
+                            {
+
+                                //do something...
+
+                            }
+
+                            else if (myItem is TimeStampedMemoVal)
+
+                            {
+
+                                string field = ((TimeStampedMemoVal)myItem).name;
+
+                                TimeStampedMemoSection[] sections = ((TimeStampedMemoVal)myItem).Section;
+
+
+
+                                foreach (TimeStampedMemoSection sec in sections)
+
+                                {
+
+                                    DateTime dt = sec.date;
+
+                                    string state = sec.state;
+
+                                    string user = sec.user;
+
+                                    string val = sec.Value;
+
+                                    //do something...
+
+                                }
+
+                            }
+
+                            else if (myItem is AttachmentsVal)
+
+                            {
+
+                                string field = ((AttachmentsVal)myItem).name;
+
+                                Attachment[] att = ((AttachmentsVal)myItem).Attachments;
+
+
+
+                                foreach (Attachment a in att)
+
+                                {
+
+                                    string name = a.name;
+
+                                    DateTime dt = a.creationDate;
+
+                                    bool bIsLink = a.link;
+
+                                    byte[] val = a.Value;
+
+                                    string descr = a.description;
+
+                                    DateTime mod = a.lastModification;
+
+                                    //do something...
+
+                                }
+
+                            }
+
+                            else if (myItem is ReferenceListVal)
+
+                            {
+
+                                string field = ((ReferenceListVal)myItem).name;
+
+                                int[] objects = ((ReferenceListVal)myItem).objectIds;
+
+                                //do something...
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    string err = golr.errorMsg;
+
+                }
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                string err = ex.Message;
+
+            }
+
+        }
+
+
+    }
+
+
 }
